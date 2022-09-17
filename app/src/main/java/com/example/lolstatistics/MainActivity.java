6b5c00491a62f483cb34b68c8a3071b89fe31542
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2022-09-17 18:40:12 -0300

    17/09 codigo dos campeoes

--- app/src/main/java/com/example/lolstatistics/MainActivity.java
+++ app/src/main/java/com/example/lolstatistics/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lolstatistics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void AbrirGuiaAatrox(View view){
+        Intent intent = new Intent(this, ActivityAatrox.class);
+        startActivity(intent);
+    }
+    public void AbrirGuiaAhri(View view){
+        Intent intent = new Intent(this, ActivityAhri.class);
+        startActivity(intent);
+    }
+    public void AbrirGuiaAkali(View view){
+        Intent intent = new Intent(this, ActivityAkali.class);
+        startActivity(intent);
+    }
+    public void AbrirGuiaAkshan(View view){
+        Intent intent = new Intent(this, ActivityAkshan.class);
+        startActivity(intent);
+    }
+    public void AbrirGuiaAlistar(View view){
+        Intent intent = new Intent(this, ActivityAlistar.class);
+        startActivity(intent);
     }
 }
