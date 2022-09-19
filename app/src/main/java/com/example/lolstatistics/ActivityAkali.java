9fb1242b95fe3a7327235b0312bb4dbb850901ca
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2022-09-19 19:43:28 -0300

    19/09 Sensor de aproximacao

--- app/src/main/java/com/example/lolstatistics/ActivityAkali.java
+++ app/src/main/java/com/example/lolstatistics/ActivityAkali.java
@@ -2,13 +2,57 @@ package com.example.lolstatistics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ActivityAkali extends AppCompatActivity {
+    private SensorManager sensorManager;
+    private Sensor proximitySensor;
+    private SensorEventListener proximitySensorListner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_akali);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if (proximitySensor == null) {
+            Toast.makeText(this, "Proxi", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        proximitySensorListner = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(sensorEvent.values[0] == 0){
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+
+                }else{
+                    getWindow().getDecorView().setBackgroundColor(Color.GRAY);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        sensorManager.registerListener(proximitySensorListner, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(proximitySensorListner);
+
     }
 }
